21f6d055b17f390d88688c11d7e2868f7b3f75c3
Developer <parth.g306@gmail.com>
2025-07-27 10:20:07 +0530

    commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,5 +14,6 @@
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
+        <div></div>
     </div>
 </x-layouts.app>
